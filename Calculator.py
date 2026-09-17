1ade06dc80397af834a0464bf904f03914dfb105
Amitis Yousefi <amitisy67@gmail.com>
2026-09-17 18:49:41 +0330

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -82,7 +82,52 @@ def times():
     expression = " × ".join(map(str, numbers))
     save_history(f"{expression} = {total}")
     return total
-Handle division by
+def division(): 
+    total = int(input("🌸 give me first number: ")) 
+    numbers.append(total) 
+ 
+    while True: 
+        num = input("🌸 give me a number: ") 
+ 
+        if num == 'done': 
+            break 
+ 
+        numbers.append(int(num)) 
+ 
+    try:
+        for num in numbers[1:]: 
+            total = total / num
+    except ZeroDivisionError:
+        print("❌ Cannot divide by zero! 🎀")
+        return
+
+    expression = " ÷ ".join(map(str, numbers)) 
+    save_history(f"{expression} = {total}") 
+ 
+    return total    
+def floor_division(): 
+    total = int(input("🌸 give me first number: ")) 
+    numbers.append(total) 
+ 
+    while True: 
+        num = input("🌸 give me a number: ") 
+ 
+        if num == 'done': 
+            break 
+ 
+        numbers.append(int(num)) 
+ 
+    try:
+        for num in numbers[1:]: 
+            total = total // num
+    except ZeroDivisionError:
+        print("❌ Cannot divide by zero! 🎀")
+        return
+
+    expression = " // ".join(map(str, numbers)) 
+    save_history(f"{expression} = {total}") 
+ 
+    return total
 def clear_history():
     with open("history.json", "w") as file:
         json.dump([], file)
